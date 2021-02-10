9b2f48f3b1958dac76a540ff4425d54462b55b93
Mark Hillin <mark.hillin@gmail.com>
2021-02-09 22:16:19 -0600

    Almost Done

--- api/server.js
+++ api/server.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require('express-session');
 
 const usersRouter = require("./users/users-router.js");
+const authRouter = require('./auth/auth-router.js');
 
 const server = express();
+
+const sessionConfig = {
+  name: 'monkey',
+  secret: 'keep it secret keep it dark',
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+    secure: false,
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: false
+}
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.use("/api/users", usersRouter);
+server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,7 +2,9 @@ const router = require("express").Router();
 
 const Users = require("./users-model.js");
 
-router.get("/", (req, res) => {
+const loginCheck = require('../auth/logged-in-check-middleware.js');
+
+router.get("/", loginCheck, (req, res) => {
   Users.find()
     .then(users => {
       res.status(200).json(users);
